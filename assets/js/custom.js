62cb37cdc6d7760be148bc89e0c2809f14e8807f
David Mensah <david.mensah@hubtel.com>
2023-01-21 02:47:10 +0000

    Set automatically generated copyright year

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,3 +2,10 @@
     Custom JS (Any custom js code you want to apply should be defined here).
 ====================================================================================*/
 
+$(document).ready(function(){
+
+    // Set the copyright year
+    var date = new Date();
+    $("#copyRightYear").html(date.getFullYear());
+  
+});

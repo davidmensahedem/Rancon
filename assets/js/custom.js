97942fb43808fe33cf9034f880618cb52ab96b58
David Mensah <david.mensah@hubtel.com>
2023-01-21 13:22:21 +0000

    Make a project tab active

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,5 +7,14 @@ $(document).ready(function(){
     // Set the copyright year
     var date = new Date();
     $("#copyRightYear").html(date.getFullYear());
+
+    // Make a project tab active
+    var projectTabs = $(".project-tab");
+    projectTabs.on("click",function(e){
+        for (let index = 0; index < projectTabs.length; index++) {
+            projectTabs[index].classList.remove("active-project-tab");
+        }
+        e.currentTarget.classList.add("active-project-tab");
+    });
   
 });
